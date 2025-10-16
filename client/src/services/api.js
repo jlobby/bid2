032ef088931d7b7f6fe481f9ebce9f528912fa2c
shai co <lobbyyehudi@gmail.com>
2025-10-16 15:18:17 +0300

    update routes to include /api prefix

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'https://bid2-1.onrender.com';
+
 
 // Create axios instance
 const api = axios.create({
